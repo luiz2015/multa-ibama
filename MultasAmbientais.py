21493b329d2c1f439ac16277d2efcb2e206a103f
Luiz Sergio Marques <luiz_sergio@icloud.com>
2019-01-29 21:47:36 -0300

    Primeira versao do programa

---
+++ MultasAmbientais.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import pandas as pd
+
+def main():
+    print("Acessando lista de multas...")
+    response = requests.get("http://dadosabertos.ibama.gov.br/dados/SICAFI/AC/Quantidade/multasDistribuidasBensTutelados.json")
+    if response.status_code == 200:
+        list_of_processes = response.json()
+        amount = len(list_of_processes['data'])
+        print ("Foram encontrados %d processos" %amount)
+        list_municipio =[]
+        list_nomeRazaoSocial =[]
+        list_valorAuto = []
+        list_dataAuto = []
+        list_situcaoDebito = []
+        categories =["Fauna", "Flora", "Pesca", "Outras"]
+        for category in categories:
+            print("Acessando multas sobre %s ..." % category)
+            for process in list_of_processes['data']:
+                if process["tipoInfracao"] == category:
+                    list_municipio.append(process["municipio"])
+                    list_nomeRazaoSocial.append(process["nomeRazaoSocial"])
+                    list_valorAuto.append(process["valorAuto"])
+                    list_dataAuto.append(process["dataAuto"])
+                    list_situcaoDebito.append(process["situcaoDebito"])
+            row = {'municipio': list_municipio, 'nomeRazaoSocial': list_nomeRazaoSocial, 'valorAuto': list_valorAuto,'dataAuto': list_dataAuto, 'situcao': list_situcaoDebito}
+            df = pd.DataFrame(row, columns=['municipio', 'nomeRazaoSocial', 'valorAuto', 'dataAuto', 'situcaoDebito'])
+            df.to_csv('%s.csv' % category)
+            print ("Multas relacionados a %s foram salvas na tabel!" % category)
+            list_municipio.clear()
+            list_nomeRazaoSocial.clear()
+            list_valorAuto.clear()
+            list_dataAuto.clear()
+            list_situcaoDebito.clear()
+    else:
+        print("Site com algum problema!")
+
+#Pede para que o programa execute a nossa função main
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,26 @@
+![Python](https://www.python.org/static/community_logos/python-logo-generic.svg)
+
+# Multa Ibama
+Um script para pegar as mutas do IBAMA, em quatro categorias, do site do governo:
+  - Flora 
+  - Fauna
+  - Pesca
+  - Outros
+  
+Pegando as informacoes:
+- Municipio
+- Nome / Razao Social
+- Valor da Infração
+- Data da infração
+- Situação do debito 
+
+Gerando Arquivo arquivos CSV, por categoria (Flora / Fauna /Pesca / Outros) 
+
+
+### Desenvolvedores
+
+ - Luiz Sérgio Marques
+
+License
+----
+**Free Software!**
